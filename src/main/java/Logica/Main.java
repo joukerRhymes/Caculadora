876479ceef574e8d78d0bfb778b75f5faedf5824
joukerRhymes <sebastian.figueroam4@gmail.com>
2023-01-08 22:17:28 -0300

    primer commit

---
+++ src/main/java/Logica/Main.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import IGU.Ventana;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Ventana ventana =  new Ventana();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
